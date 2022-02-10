6f0a928170bb4c643cf45d5fd269c0c39fa6d5c4
Long Hoang <dangkikhoqua191@gmail.com>
2022-02-10 15:27:02 +0700

    todo app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ListToDo from './Component/ListToDo'
+import { useState, useRef} from 'react'
+import {v4 as uuidv4} from 'uuid'
 
-const Default = (props)=>{
-  return(
-    <h1>{props.text}</h1>
-  )
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const todoName = useRef();
+
+  const toggleTodo = (id) =>{
+      const newTodos = [...todos];
+      const todo = newTodos.find(todo => todo.id === id)
+      todo.complete = !todo.complete;
+      setTodos(newTodos); 
+  }
+
+  const handleAddToDo = (e)=>{
+      const name = todoName.current.value;
+      if (name.length <=0 ) return
+      setTodos(prevTodos =>{
+        return [...prevTodos, {id:uuidv4(), name: name, complete: false}]
+      })
+
+      todoName.current.value = null;
+
+  }
+
+  const handleRemove = ()=>{
+      const newTodos = [...todos];
+      const listNotComplete = newTodos.filter(todo => todo.complete === false)
+      setTodos(listNotComplete)
+  }
+
+
+  return (
+    <div>
+      <ListToDo  todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoName} type="text"></input>
+      <button onClick={handleAddToDo}>Add To Do</button>
+      <button onClick={handleRemove} >Clear Completed</button>
+      <div>{todos.filter(todo => todo.complete === false).length} left to do</div>
+    </div>
+  ) 
 }
-
-const App = (props)=>{
-    
-    return(
-      <Default text = 'blue'></Default>
-    )
-}
-
-export default App;
